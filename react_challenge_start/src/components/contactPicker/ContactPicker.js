301a549b33f775050930574cf29ab5fe055bd2f0
Leprekus <leprekus@yahoo.com>
2022-10-02 15:44:24 -0700

    fix : contact name appears when adding appointment

--- react_challenge_start/src/components/contactPicker/ContactPicker.js
+++ react_challenge_start/src/components/contactPicker/ContactPicker.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useAppointmentInfoUpdateContext } from "../../Context/AppointmentInfoContext";
 
-export const ContactPicker = ( { contacts }) => {
-
+export const ContactPicker = ({ contacts }) => {
+  
   const updateAppointmentInfo = useAppointmentInfoUpdateContext()
+  const selectRef = useRef(null)  
+  const optionRef = useRef(null)
+  const currentOption = optionRef.current
+
+  useEffect(() => {
+    const selectRefObject = {
+      target: {
+        name:'Contact',
+        value: selectRef.current?.value
+      }
+    }
+    selectRef.current?.addEventListener('onSelect', updateAppointmentInfo(selectRefObject))
+  
+    return () => selectRef.current?.removeEventListener('onSelect', updateAppointmentInfo)
+  }, [selectRef.current?.value])
+  
 
   return (
-      <select name='Contact' onChange={updateAppointmentInfo} >
+    <select 
+      name='Contact' 
+      ref={selectRef}
+      >
     {
       contacts.map((contact, index) => (
       <option 
       key={`${contact.Name}_${index}`} 
       value={contact.Name}
+      name='Contact'
       >{contact.Name}</option>
       ))
     }
